7affa7a8413dbf508dce0e28cfbaf16089320d10
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2024-12-09 22:19:05 +0100

    cleanup

--- src/campaign/dto/updated-campaign.input.ts
+++ src/campaign/dto/updated-campaign.input.ts
@@ -1,10 +1,7 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
 import { MaxLength, MinLength } from 'class-validator';
-import { AtLeastOneField } from '../../utils/at-least-one.decorator';
 
 @InputType()
-// TODO apply this:
-// @AtLeastOneField(['name', 'dateStart', 'dateEnd'])
 export class UpdatedCampaignInput {
   @Field(() => ID)
   id: string;
