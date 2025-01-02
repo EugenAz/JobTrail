1d87bb1de91b15982c01b6a1715f5a0597e48584
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-01-02 16:06:58 +0100

    feat: filter by status

--- apps/react-client/src/pages/campaign.tsx
+++ apps/react-client/src/pages/campaign.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router';
+import { Link, useParams, useSearchParams } from 'react-router';
 import { formatDate } from '@job-trail/dates';
 
 import { useApplicationDeleter } from '../graphql/use-application-deleter';
@@ -8,9 +8,17 @@ import { ButtonLink } from '../components/atoms/button-link';
 import { LoadingErrorHandler } from '../components/loading-error-handler';
 
 import styles from './campaign.module.css';
+import { Button } from '../components/atoms/button';
+import { useEffect, useState } from 'react';
+import { ApplicationStatus, IApplicationModel } from '@job-trail/types';
+
+const STATUS_FILTER_KEY = 'statusFilter';
 
 export const Campaign = () => {
   const { campaignId } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filterStatus = searchParams.get(STATUS_FILTER_KEY) as ApplicationStatus;
+  const [applications, setApplications] = useState<IApplicationModel[]>([]);
 
   if (!campaignId) {
     throw new Error('campaign ID is missing');
@@ -25,7 +33,21 @@ export const Campaign = () => {
   // TODO sort by date and status
 
   const campaign = data?.campaign;
-  const applications = campaign?.applications;
+  useEffect(() => {
+    let apps: IApplicationModel[];
+
+    if (campaign?.applications) {
+      apps = campaign?.applications;
+
+      if (filterStatus === ApplicationStatus.OPEN) {
+        apps = apps?.filter(
+          (a) => a.status.toLowerCase() === ApplicationStatus.OPEN
+        );
+      }
+
+      setApplications(apps);
+    }
+  }, [campaign?.applications, filterStatus]);
 
   const handleDeleteClick = async (id: string) => {
     if (window.confirm('Are you sure?')) {
@@ -43,6 +65,17 @@ export const Campaign = () => {
 
   // TODO search by role names and company names
 
+  const handleFilterOpenToggleClick = () => {
+    setSearchParams((prev) => {
+      if (filterStatus === ApplicationStatus.OPEN) {
+        prev.delete(STATUS_FILTER_KEY);
+      } else {
+        prev.set(STATUS_FILTER_KEY, ApplicationStatus.OPEN);
+      }
+      return prev;
+    });
+  };
+
   return (
     <LoadingErrorHandler
       loading={loading || mutationLoading}
@@ -50,9 +83,17 @@ export const Campaign = () => {
     >
       <MainHeading>{campaign?.name}</MainHeading>
 
-      <ButtonLink to={`/campaign/${campaignId}/new-application`}>
-        + Add application
-      </ButtonLink>
+      <div className="flex gap-4">
+        <ButtonLink to={`/campaign/${campaignId}/new-application`}>
+          + Add application
+        </ButtonLink>
+
+        <Button onClick={handleFilterOpenToggleClick}>
+          {filterStatus === ApplicationStatus.OPEN
+            ? 'Show all'
+            : 'Show only open'}
+        </Button>
+      </div>
 
       <table className="mt-4 rounded-t-lg overflow-hidden">
         <thead className="text-center">
