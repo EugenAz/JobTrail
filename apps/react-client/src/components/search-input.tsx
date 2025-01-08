7ce3d17a3dc387019ab6d84ec1f5cb7eee359de1
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-01-08 10:47:42 +0100

    feat: focus on the search input on cmd+k

--- apps/react-client/src/components/search-input.tsx
+++ apps/react-client/src/components/search-input.tsx
@@ -1,4 +1,4 @@
-import { FC, ChangeEvent } from 'react';
+import { FC, ChangeEvent, useEffect } from 'react';
 
 interface SearchInputProps {
   handleSearch: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -9,10 +9,22 @@ export const SearchInput: FC<SearchInputProps> = ({
   handleSearch,
   searchTerm,
 }) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.metaKey && e.key === 'k') {
+        e.preventDefault();
+        const input = document.querySelector('input');
+        input?.focus();
+      }
+    };
+    document.body.addEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <input
       className="border-gray-600 border-2 rounded-md p-2"
       placeholder="Search..."
+      id="search-input"
       onChange={handleSearch}
       value={searchTerm}
     />
