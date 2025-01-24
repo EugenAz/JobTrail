dfbe21308dc94a9aa4b9870f4238d249586ee6dd
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-01-24 17:19:40 +0100

    chore: generate migrations

---
+++ apps/server/src/datasource.config.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from './env';
+
+export default new DataSource({
+  type: 'postgres',
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  synchronize: false,
+  logging: true,
+  entities: [join(__dirname, '**/*.entity.{js,ts}')],
+  migrations: [join(__dirname, '../migrations/*.{js,ts}')],
+});
