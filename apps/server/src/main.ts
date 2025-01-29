673168732fc038be7a52751a580cad7a46c9f00c
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-01-29 13:28:17 +0100

    chore(server): teesting the k8s release

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './env';
+import { PORT, ENVIRONMENT } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   await app.listen(PORT);
-  Logger.log(`🚀 Application is running on: http://localhost:${PORT}/graphql`);
+  Logger.log(
+    `🚀 Application is running in ${ENVIRONMENT} mode on: http://localhost:${PORT}/graphql`
+  );
 }
 
 bootstrap();
