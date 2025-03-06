e34d7fc82e11cc92250699abb4c84d1af973e96f
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-03-06 10:25:50 +0100

    Beutify the today date

--- libs/dates/src/lib/get-today.ts
+++ libs/dates/src/lib/get-today.ts
@@ -1,4 +1,9 @@
 export const getToday = (): string => {
   const today = new Date();
-  return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
 };
