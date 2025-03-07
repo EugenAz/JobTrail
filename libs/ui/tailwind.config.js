19285eb0bd92b928b32aa802144ef69fabb01b4e
Eugen Aznaurov <3042888+EugenAz@users.noreply.github.com>
2025-03-07 22:56:21 +0100

    Set up shadcn

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,16 +1,17 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const TailwindConfig = require('../../libs/ui/tailwind.config');
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  ...TailwindConfig,
   content: [
-    ...TailwindConfig.content,
     join(
       __dirname,
-      '{src,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
